fccdd7efe438c1e6dfbcf265bfbf7d8ad67752ea
jyliana <jyliana@gmail.com>
2019-02-11 00:20:43 +0200

    refactoring, adding command 'help'

---
+++ src/ua/com/juja/sqlcmd/controller/command/Help.java
@@ -0,0 +1,32 @@
+package ua.com.juja.sqlcmd.controller.command;
+
+import ua.com.juja.sqlcmd.view.View;
+
+public class Help implements Command {
+    private View view;
+
+    public Help(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public boolean canProcess(String command) {
+        return command.equals("help");
+    }
+
+    @Override
+    public void process(String command) {
+        view.write("Существующие команды: ");
+        view.write("\tlist");
+        view.write("\t\tдля получения списка всех таблиц базы, к которой подключились");
+
+        view.write("\tfind|tableName");
+        view.write("\t\tдля получения содержимого таблицы 'tableName'");
+
+        view.write("\thelp");
+        view.write("\t\tдля вывода этого списка на экран");
+
+        view.write("\texit");
+        view.write("\t\tдля выходя из программы");
+    }
+}
