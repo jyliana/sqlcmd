54f4d8f4583042d0e46fb52051702f55c7b63496
jyliana <jyliana@gmail.com>
2019-02-19 23:00:32 +0200

    adding and fixing integration tests

--- test/ua.com.juja.sqlcmd/integration/IntegrationTest.java
+++ test/ua.com.juja.sqlcmd/integration/IntegrationTest.java
@@ -1,6 +1,6 @@
 package ua.com.juja.sqlcmd.integration;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import ua.com.juja.sqlcmd.controller.Main;
 
@@ -13,16 +13,27 @@ import static org.junit.Assert.assertEquals;
 
 public class IntegrationTest {
 
-    private static ConfigurableInputStream in;
-    private static ByteArrayOutputStream out;
+    private ConfigurableInputStream in;
+    private ByteArrayOutputStream out;
 
-    @BeforeClass
-    public static void setup() {
+    @Before
+    public void setup() {
         out = new ByteArrayOutputStream();
         in = new ConfigurableInputStream();
 
         System.setIn(in);
         System.setOut(new PrintStream(out));
+    }
+
+
+    public String getData() {
+        try {
+            String result = new String(out.toByteArray(), "UTF-8");
+            out.reset();
+            return result;
+        } catch (UnsupportedEncodingException e) {
+            return e.getMessage();
+        }
     }
 
     @Test
@@ -52,13 +63,6 @@ public class IntegrationTest {
                 "До скорой встречи!\r\n", getData());
     }
 
-    public String getData() {
-        try {
-            return new String(out.toByteArray(), "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            return e.getMessage();
-        }
-    }
 
     @Test
     public void testExit() {
@@ -88,6 +92,142 @@ public class IntegrationTest {
         assertEquals("Привет, юзер!\r\n" +
                 "Введи, пожалуйста, имя базы данных, имя пользователя и пароль в формате: connect|database|userName|password\r\n" +
                 "Вы не можете пользоваться командой 'list', пока не подключитесь к базе с помощью команды connect|databaseName|userName|password\r\n" +
+                "Введи команду (или help для помощи):\r\n" +
+                "До скорой встречи!\r\n", getData());
+    }
+
+    @Test
+    public void testFindWithoutConnect() {
+        //given
+        in.add("find|users");
+        in.add("exit");
+
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("Привет, юзер!\r\n" +
+                "Введи, пожалуйста, имя базы данных, имя пользователя и пароль в формате: connect|database|userName|password\r\n" +
+                "Вы не можете пользоваться командой 'find|users', пока не подключитесь к базе с помощью команды connect|databaseName|userName|password\r\n" +
+                "Введи команду (или help для помощи):\r\n" +
+                "До скорой встречи!\r\n", getData());
+    }
+
+    @Test
+    public void testUnsupported() {
+        //given
+        in.add("unsupported");
+        in.add("exit");
+
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("Привет, юзер!\r\n" +
+                "Введи, пожалуйста, имя базы данных, имя пользователя и пароль в формате: connect|database|userName|password\r\n" +
+                "Вы не можете пользоваться командой 'unsupported', пока не подключитесь к базе с помощью команды connect|databaseName|userName|password\r\n" +
+                "Введи команду (или help для помощи):\r\n" +
+                "До скорой встречи!\r\n", getData());
+    }
+
+    @Test
+    public void testUnsupportedAfterConnect() {
+        //given
+        in.add("connect|sqlcmd|postgres|123456");
+        in.add("unsupported");
+        in.add("exit");
+
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("Привет, юзер!\r\n" +
+                "Введи, пожалуйста, имя базы данных, имя пользователя и пароль в формате: connect|database|userName|password\r\n" +
+                // connect
+                "Успешно подключились.\r\n" +
+                "Введи команду (или help для помощи):\r\n" +
+                // unsupported
+                "Несуществующая команда: unsupported\r\n" +
+                "Введи команду (или help для помощи):\r\n" +
+                // exit
+                "До скорой встречи!\r\n", getData());
+    }
+
+    @Test
+    public void testListAfterConnect() {
+        //given
+        in.add("connect|sqlcmd|postgres|123456");
+        in.add("list");
+        in.add("exit");
+
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("Привет, юзер!\r\n" +
+                "Введи, пожалуйста, имя базы данных, имя пользователя и пароль в формате: connect|database|userName|password\r\n" +
+                // connect
+                "Успешно подключились.\r\n" +
+                "Введи команду (или help для помощи):\r\n" +
+                // list
+                "[news, users, test]\r\n" +
+                "Введи команду (или help для помощи):\r\n" +
+                // exit
+                "До скорой встречи!\r\n", getData());
+    }
+
+    @Test
+    public void testFindAfterConnect() {
+        //given
+        in.add("connect|sqlcmd|postgres|123456");
+        in.add("find|test");
+        in.add("exit");
+
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("Привет, юзер!\r\n" +
+                "Введи, пожалуйста, имя базы данных, имя пользователя и пароль в формате: connect|database|userName|password\r\n" +
+                // connect
+                "Успешно подключились.\r\n" +
+                "Введи команду (или help для помощи):\r\n" +
+                // find|test
+                "--------------------------\r\n" +
+                " | id | \r\n" +
+                "--------------------------\r\n" +
+                "--------------------------\r\n" +
+                "Введи команду (или help для помощи):\r\n" +
+                // exit
+                "До скорой встречи!\r\n", getData());
+    }
+
+    @Test
+    public void testConnectAfterConnect() {
+        //given
+        in.add("connect|sqlcmd|postgres|123456");
+        in.add("list");
+        in.add("connect|postgres|postgres|123456");
+        in.add("list");
+        in.add("exit");
+
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("Привет, юзер!\r\n" +
+                "Введи, пожалуйста, имя базы данных, имя пользователя и пароль в формате: connect|database|userName|password\r\n" +
+                // connect
+                "Успешно подключились.\r\n" +
+                "Введи команду (или help для помощи):\r\n" +
+                // list
+                "[news, users, test]\r\n" +
+                "Введи команду (или help для помощи):\r\n" +
+                // connect to other DB
+                "Успешно подключились.\r\n" +
+                "Введи команду (или help для помощи):\r\n" +
+                // list
+                "[test2]\r\n" +
                 "Введи команду (или help для помощи):\r\n" +
                 "До скорой встречи!\r\n", getData());
     }
